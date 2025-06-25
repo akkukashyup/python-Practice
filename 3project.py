ad59554447b508c069c755ee6a97cb37592a6c51
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-25 22:52:41 +0530

    Add files via upload

---
+++ 3project.py
@@ -0,0 +1,17 @@
+#WAP to input 2 floating point numbers & print their average.
+
+# Average = Sum of all values / Number of values
+
+val1 = float(input("enter your value :"))
+
+val2 = float(input("enter your value :"))
+
+val3 = float(input("number of value :"))
+
+sum  = val1+val2 
+
+
+print("sum of all values :", sum)
+
+print("average value :" , sum/val3)
+
