383cba69868db337b6c345d173926170e5e492c0
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-26 23:09:07 +0530

    Add files via upload

---
+++ 4project.py
@@ -0,0 +1,9 @@
+
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False.
+
+a = int(input("enter value a :"))
+b = int(input("enter value b :"))
+
+
+print(a>=b)
