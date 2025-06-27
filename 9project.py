70e79ddb950709741ef716630ac47c92dcdf91d2
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-27 23:01:57 +0530

    Add files via upload

---
+++ 9project.py
@@ -0,0 +1,8 @@
+# WAP to check if a number is a multiple of 7 or not.
+
+num = int(input("enter value : "))
+
+if( num % 7 == 0):
+   print("mutliple of 7")
+else:
+    print("not multiple of 7")    
