af48e7893b7f598fbd304f04bfc461677ade4087
akkukashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-27 23:02:18 +0530

    Add files via upload

---
+++ 10project.py
@@ -0,0 +1,14 @@
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+
+
+movies = []
+
+mov1 = input("enter 1st moive: ")
+mov2 = input("enter 2nd moive: ")
+mov3 = input("enter 3rd moive: ")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+
+print(movies)
