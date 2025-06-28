8a6431dd5cc0badb9b53b522afebe92e38b3f5c6
Akanksha Kashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-28 22:57:46 +0530

    Add files via upload

---
+++ 11project.py
@@ -0,0 +1,12 @@
+# WAP to count the number of students with the “A” grade in the following tuple.
+
+Grade = ("c", "D", "A", "A", "B", "B", "A")
+
+print(Grade.count("A"))
+
+
+# Store the above values in a list & sort them from “A” to “D”
+
+grade = ["c", "D", "A", "A", "B", "B", "A"]
+grade.sort()
+print(grade)
