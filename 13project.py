deefa522356007f8cbfa2577ffa0da34198f73f6
Akanksha Kashyup <123484615+akkukashyup@users.noreply.github.com>
2025-06-29 22:48:49 +0530

    Add files via upload

---
+++ 13project.py
@@ -0,0 +1,4 @@
+# You are given a list of subjects for students. Assume one classroom is required for 1
+# subject. How many classrooms are needed by all students.
+# ”python”,“java”,,“python”,“javascript”,“java”,“python”,“java”,“C++”,“C”
+
